89dfdca185e954341823943bbec6174b1eb00b48
wellbell <uok02018496@naver.com>
2021-05-04 01:41:46 +0900

    구조 패턴 - 브릿지 패턴 연습

---
+++ src/structural/bridge/BridgePatternDemo.java
@@ -0,0 +1,11 @@
+package structural.bridge;
+
+public class BridgePatternDemo {
+    public static void main(String[] args) {
+        Shape tri = new Triangle(new RedColor());
+        tri.applyColor();
+
+        Shape pent = new Pentagon(new GreenColor());
+        pent.applyColor();
+    }
+}
